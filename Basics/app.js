6de0a6ae4daa9eb03f242a50398d3378683416a4
RamzRjs <60762747+RameesaRijas@users.noreply.github.com>
2022-10-17 21:34:19 -0400

    assignments and type inference

--- Basics/app.js
+++ Basics/app.js
@@ -1,7 +1,14 @@
-var add = function (num1, num2) {
-    return num1 + num2;
+var add = function (num1, num2, showResult, phrase) {
+    var result = num1 + num2;
+    if (showResult) {
+        console.log(phrase + result);
+    }
+    else {
+        return num1 + num2;
+    }
 };
 var number1 = 5;
 var number2 = 4;
-var result = add(number1, number2);
-console.log(result);
+var printResult = true;
+var resultPhrase = "Result is: ";
+var result = add(number1, number2, printResult, resultPhrase);
--- Basics/app.ts
+++ Basics/app.ts
@@ -1,10 +1,17 @@
-const add = (num1: number, num2: number) => {
-  return num1 + num2;
+const add = (num1: number, num2: number, showResult: boolean, phrase: string) => {
+  const result = num1 + num2;
+  if (showResult) {
+    console.log(phrase + result)
+  } else {
+    return num1 + num2;
+  } 
 }
 
 const number1 = 5;
 const number2 = 4;
+const printResult = true;
+const resultPhrase = "Result is: "
 
-const result = add(number1, number2);
-console.log(result);
+
+const result = add(number1, number2, printResult, resultPhrase);
 
