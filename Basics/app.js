1130c84e34e73eb40d5529916508412ee3878112
RamzRjs <60762747+RameesaRijas@users.noreply.github.com>
2022-10-18 20:46:22 -0400

    function type and callbacks

--- Basics/app.js
+++ Basics/app.js
@@ -1,7 +1,17 @@
 var add1 = function (n1, n2) {
     return n1 + n2;
 };
+//void return void
 var printResult1 = function (num) {
     console.log("result :" + num);
 };
+var addAndHandle = function (num1, num2, cb) {
+    var result = num1 + num2;
+    cb(result);
+};
 printResult1(add1(5, 6));
+//let combineValues: Function;
+var combineValues;
+combineValues = add1;
+console.log(combineValues(6, 7));
+addAndHandle(10, 20, function (result) { console.log(result); });
