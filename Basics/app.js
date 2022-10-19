b91d698ccdc2a57a83d768b9f55950c5769c7a81
RamzRjs <60762747+RameesaRijas@users.noreply.github.com>
2022-10-18 21:05:23 -0400

    unknown & never types

--- Basics/app.js
+++ Basics/app.js
@@ -1,17 +1,16 @@
-var add1 = function (n1, n2) {
-    return n1 + n2;
+var userInput;
+var userName;
+userInput = 5;
+userInput = "max";
+//unknown type
+//userName = userInput will give error
+//so
+//unknown type is not assignable
+//unknown is strict, so first want to check the type of variable and then assign
+if (typeof userInput === 'string') {
+    userName = userInput;
+}
+var generateError = function (message, code) {
+    throw { message: message, errorCode: code };
 };
-//void return void
-var printResult1 = function (num) {
-    console.log("result :" + num);
-};
-var addAndHandle = function (num1, num2, cb) {
-    var result = num1 + num2;
-    cb(result);
-};
-printResult1(add1(5, 6));
-//let combineValues: Function;
-var combineValues;
-combineValues = add1;
-console.log(combineValues(6, 7));
-addAndHandle(10, 20, function (result) { console.log(result); });
+generateError("An error occured", 500);
--- Basics/types.ts
+++ Basics/types.ts
@@ -14,5 +14,9 @@ if(typeof userInput === 'string') {
   userName = userInput
 }
 
+const generateError = (message: string, code: number) => {
+  throw {message : message, errorCode: code};
+}
 
+generateError("An error occured", 500)
 
